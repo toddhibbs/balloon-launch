fa356db5250f659ebeba3b0b63f0b0a8b42b39bf
Todd Hibbs <todd@toddhibbs.com>
2016-08-12 19:15:49 -0700

    Updated script to write csv instead of json

--- logger.py
+++ logger.py
@@ -8,9 +8,10 @@ count = 0
 max_count = 5000
 next_call = time.time()
 timestr = time.strftime("%Y%m%d-%H%M%S")
-fo = open('data-' + timestr + '.json', 'w', 0)
+fo = open('data-' + timestr + '.csv', 'w', 0)
 
-fo.write('{ "data": [')
+header = ['timestamp','orientation_radians','orientation_degrees','compass','compass_raw','gyroscope','accelerometer','temp','temp_humidity','temp_pressure','pressure','humidity'];
+fo.write(','.join(header))
 
 def logger():
   global next_call
@@ -23,26 +24,38 @@ def logger():
   if count < max_count:
     threading.Timer( next_call - time.time(), logger ).start()
 
-    if count > 0:
-      fo.write(',')
 
-    data['timestamp'] = datetime.now().isoformat('T')
-    data['orientation_radians'] = sense.orientation_radians
-    data['orientation_degrees'] = sense.orientation
-    data['compass'] = sense.compass
-    data['compass_raw'] = sense.compass_raw
-    data['gyroscope'] = sense.gyroscope
-    data['accelerometer'] = sense.accelerometer
-    data['temp'] = sense.temp
-    data['temp_humidity'] = sense.get_temperature_from_humidity()
-    data['temp_pressure'] = sense.get_temperature_from_pressure()
-    data['pressure'] = sense.pressure
-    data['humidity'] = sense.humidity
+    data = [
+      datetime.now().isoformat('T'),
+      sense.orientation_radians,
+      sense.orientation,
+      sense.compass,
+      sense.compass_raw,
+      sense.gyroscope,
+      sense.accelerometer,
+      sense.temp,
+      sense.get_temperature_from_humidity(),
+      sense.get_temperature_from_pressure(),
+      sense.pressure,
+      sense.humidity
+    ]
+    fo.write(','.join(data))
 
-    json.dump(data, fo)
+    # data['timestamp'] = datetime.now().isoformat('T')
+    # data['orientation_radians'] = sense.orientation_radians
+    # data['orientation_degrees'] = sense.orientation
+    # data['compass'] = sense.compass
+    # data['compass_raw'] = sense.compass_raw
+    # data['gyroscope'] = sense.gyroscope
+    # data['accelerometer'] = sense.accelerometer
+    # data['temp'] = sense.temp
+    # data['temp_humidity'] = sense.get_temperature_from_humidity()
+    # data['temp_pressure'] = sense.get_temperature_from_pressure()
+    # data['pressure'] = sense.pressure
+    # data['humidity'] = sense.humidity
+
 
   else:
-    fo.write(']}')
     fo.close()
 
   count += 1
